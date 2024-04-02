fa5ad841266ff452f89453841b116555261f6be1
willian <willian.cacoelho@gmail.com>
2024-04-01 23:31:05 -0300

    feat: create AuthContext and reducer

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,14 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { createContext, useState } from "react";
-
-type AuthInfo = {
-  accessToken: string;
-  refreshToken: string;
-};
+import { createContext, useReducer } from "react";
 
 export interface AuthContextType {
-  auth: any;
-  setAuth: any;
+  state: any;
+  dispatch: React.DispatchWithoutAction;
 }
 
 export const AuthContext = createContext({} as AuthContextType);
@@ -17,10 +12,53 @@ interface AuthContextProviderType {
   children: React.ReactNode;
 }
 
+enum CountActionKind {
+  SIGN_IN = "SIGN_IN",
+  SIGN_OUT = "SIGN_OUT",
+}
+
+interface AuthState {
+  refreshToken: string;
+  accessToken: string;
+}
+
+interface AuthPayload {
+  refreshToken: string;
+  accessToken: string;
+}
+
+interface AuthAction {
+  type: CountActionKind;
+  payload: AuthPayload;
+}
+
+const authReducer = ((state: AuthState, action: AuthAction) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CountActionKind.SIGN_IN:
+      return {
+        refreshToken: payload.refreshToken,
+        accessToken: payload.accessToken,
+      };
+    case CountActionKind.SIGN_OUT:
+      return {
+        refreshToken: null,
+        accessToken: null,
+      };
+    default:
+      return state;
+  }
+}) as React.ReducerWithoutAction<any>;
+
 export const AuthContextProvider = ({ children }: AuthContextProviderType) => {
-  const [auth, setAuth] = useState<AuthInfo | null>(null);
+  const [state, dispatch] = useReducer(authReducer, {
+    accessToken: null,
+    refreshToken: null,
+  });
+
   return (
-    <AuthContext.Provider value={{ auth, setAuth }}>
+    <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
     </AuthContext.Provider>
   );
