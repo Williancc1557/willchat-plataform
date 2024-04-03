cc50cd63fe4af22a2cc0084948eb2d8376f12154
willian <willian.cacoelho@gmail.com>
2024-04-03 16:15:55 -0300

    feat: change file type tsx to ts

--- src/hooks/useSignUp.ts
+++ src/hooks/useSignUp.ts
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { UseAuthContext } from "./UseAuthContext";
+import { UseAuthContext } from "./useAuthContext";
 import { axiosConfig } from "../utils/axiosConfig";
 import { AuthActionKind } from "../contexts/AuthContext";
 
